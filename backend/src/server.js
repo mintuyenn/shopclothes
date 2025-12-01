98e55faf90a99b5e7092d035b58e91fc92a05356
mintuyenn <caotuyen281005@gmail.com>
2025-12-01 21:36:56 +0700

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,8 @@ export const upload = multer({ storage });
 
 // ------------------- MIDDLEWARE -------------------
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 
 // ------------------- ROUTES EXISTING -------------------
 app.use("/api/auth", authRoutes);
